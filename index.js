a196698788808c8a0b1a47e2f8a1ec4f291a8ccd
DuloeW <duloekeren@gmail.com>
2022-09-23 17:50:14 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+
+client.on('createMessage', () => {
+  return 'berhasi'
+});
+
+
+// Login to Discord with your client's token
+client.login("OTc2NTA5MDk1MTE5MDQ0NjA4.GkovpX.jJlVXJJXFK93X0wTl0jBYppmUky4yMjPmW0NEY");
